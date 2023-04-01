0fba11cfcbe607fee90843fc22c53dd78e6c0639
SapkotaRajat <95966103+SapkotaRajat@users.noreply.github.com>
2023-04-01 18:53:35 +0545

    Added video section Added logo some js for logo in light mode and dark mode youtube video grid added in video section

---
+++ app.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('theme-button');
+    const logoImg = document.querySelector('#logo img');
+    let currentTheme = 'light'; // set the initial theme to light
+  
+    btn.addEventListener('click', () => {
+      if (currentTheme === 'light') {
+        // switch to dark mode
+        currentTheme = 'dark';
+        logoImg.src = './assets/img/CreatorsLogo.png';
+      } else {
+        // switch to light mode
+        currentTheme = 'light';
+        logoImg.src = './assets/img/CreatorsLogoblack.png';
+      }
+    });
+  });
+  
+
+  
